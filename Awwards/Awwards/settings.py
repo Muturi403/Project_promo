c01fd00b062f0af9a088683ec1a86f7a0d204def
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-20 11:29:22 +0300

    this will set up the apps in the sttings.py file

--- Awwards/Awwards/settings.py
+++ Awwards/Awwards/settings.py
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolios',
+    'bootstrap4',
+    'tinymce',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
