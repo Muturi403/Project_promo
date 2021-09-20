757335578600f4e014516fc69ec7dfa21163e1fa
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-20 11:46:30 +0300

    this will import the cloudinary , HTML fields and users

--- portfolios/models.py
+++ portfolios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
 # Create your models here.
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db.models import Q
+from cloudinary.models import CloudinaryField
