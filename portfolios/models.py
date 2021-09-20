8fe01da1afe35a12d1f27cce7edb2ca5c2db7fb0
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-20 11:49:09 +0300

    this will add the user profile models

--- portfolios/models.py
+++ portfolios/models.py
@@ -46,3 +46,24 @@ class Projects(models.Model):
 
   class Meta:
     ordering = ['-id']
+
+
+class Profile(models.Model):
+  # profilePic = models.ImageField(upload_to='userProfile/', default='userProfile/test.png')
+  profilePic = CloudinaryField('userProfile/', default='userProfile/test.png')
+  username = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+  bio = HTMLField(blank=True, null=True)
+  phone = models.IntegerField(blank=True, null=True)
+  count = models.IntegerField(default=0, null=True, blank=True)
+
+  @receiver(post_save, sender=User)
+  def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+      Profile.objects.create(username=instance)
+
+  @receiver(post_save, sender=User)
+  def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+  def __str__(self):
+    return self.username.username
