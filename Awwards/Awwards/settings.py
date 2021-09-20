ec7124f51dacca7851c58acdd62e2628c0dfe1a1
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-20 11:39:24 +0300

    this will add the django heroku

--- Awwards/Awwards/settings.py
+++ Awwards/Awwards/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
@@ -127,3 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+cloudinary.config( 
+  cloud_name = config('CLOUD_NAME'), 
+  api_key = config('API_KEY'), 
+  api_secret = config('API_SECRET') 
+)
+
+
+django_heroku.settings(locals())
