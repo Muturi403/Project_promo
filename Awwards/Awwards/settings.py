7ee5585801b3855af46507726df7b3f9b0fa17a9
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-20 11:42:15 +0300

    this will install and import decouple

--- Awwards/Awwards/settings.py
+++ Awwards/Awwards/settings.py
@@ -15,7 +15,8 @@ import django_heroku
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-
+import dj_database_url
+from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
