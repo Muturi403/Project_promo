83831a027e5556b3768b56fd9aa65c36ec16f807
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-20 11:27:51 +0300

    this will add the url patterns

--- Awwards/Awwards/urls.py
+++ Awwards/Awwards/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.contrib.auth import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('portfolios.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
